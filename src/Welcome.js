06fce1fc423a4eb18d5d428924933ccd6b530fdd
shazia-963 <119720277+shazia-963@users.noreply.github.com>
2024-03-01 19:37:04 +0530

    Update Welcome.js

--- src/Welcome.js
+++ src/Welcome.js
@@ -6,16 +6,19 @@ import "./welcome.css";
 import "./Welcome.js";
 function Welcome() {
     return (
-      <div className="items">
-           <nav className="navbar" >
-                <ul className="Item-2" class="responsive" width={1200} >
-                    <h3>Web-based E-learning Program</h3>
+       <div className="navbar" >
+         <div className="logo">Web-based E-learning Program</div>
+                <ul>
+                 <input type="checkbox" id="check"></input>
+                 <label for="check" className="hamburger">&#9776;</label>
+                 <div className="menu">
                     <li><Link to="/Events" ><b>Events</b></Link></li>
                     <li><Link to="/Portal" ><b>Portal</b></Link></li>
                     <li><Link to="/search" ><b>Search</b></Link></li>
-                    <li><Link to="/" className="btn btn-danger"><b><i class="fa-solid fa-right-from-bracket"></i></b></Link></li>
-                  </ul>
-            </nav>
+                    <li><Link to="/">logout  <i class="fa-solid fa-right-from-bracket"></i></Link></li>
+                 </div>          
+              </ul>
+            <div className="abc">
             <img src={classroom} width={1200} height={700} class="responsive"alt="abc" />
         </div>
     )
